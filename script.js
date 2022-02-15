38dd73f327fbe90d6f2c2acdd1a81f175ae8bfac
Karthik <karthikossu@gmail.com>
2022-02-15 18:07:19 +0530

    completed  the rps project

--- script.js
+++ script.js
@@ -1,86 +1,202 @@
-/* function asking the user for the prefered item */
-function chance(){
-    let x = prompt("Please choose the weapon of your choice: ");
-    const player = x.toUpperCase();
-    console.log(player);
-    return player;
-}
-                                                                                                                 
-/*function to randomly select the computer's prefered choice of  item */
-function computerPlay(){
-    const sign = ["ROCK", "PAPER", "SCISSORS"];
-    const  randomElement = sign[Math.floor(Math.random()* sign.length)];
-    console.log(randomElement);
-    return randomElement;
-} 
-/* variable for scorecard*/
 let player = 0;
 let computer = 0;
 
-/*game play of rock paper scissors */
-function playRound(playerSelection,  computerSelection){
-    if(playerSelection === computerSelection){
-        console.log("Match draw");
-        console.log("No change in score");
-    } else if (playerSelection == "ROCK" && computerSelection == "PAPER"){
-        console.log("computer Wins.");
+
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", function playRound(playerSelection, computerSelection){
+
+    if(player == 5 ){
+        return;
+        
+    }else if (computer == 5 ) {
+         return;
+         
+    }
+    
+    
+    playerSelection = "ROCK";
+    console.log(`player:${playerSelection}`);
+    const sign = ["ROCK", "PAPER", "SCISSORS"];
+    const  randomElement = sign[Math.floor(Math.random()* sign.length)];
+    console.log(`computer:${randomElement}`);
+    computerSelection = randomElement;
+
+    let chance = document.querySelector(".chances");
+    chance.createElement = "p";
+    
+    
+   if(computerSelection === "ROCK"){
+        chance.textContent = "It's a tie. You both chose rock";
+        chance.appendChild = "chance";
+    }else if(computerSelection == "PAPER"){
+        chance.textContent = "You lose! paper beats rock";
+        chance.appendChild = "chance";;
         computer = computer + 1;
-        console.log(computer);
-        return computer;
-    }else if (playerSelection =="PAPER" && computerSelection == "SCISSORS"){
-        console.log("computer Wins");
-        computer = computer + 1;
-        console.log(computer);
-        return computer;
-    }else  if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
-        console.log("computer Wins")
-        computer = computer + 1;
-        console.log(computer);
-        return computer;
-    }else if (playerSelection == "PAPER" && computerSelection == "ROCK"){
-        console.log("You Win.");
-        player = player + 1;
-        console.log(player);
-        return player;
-    }else if (playerSelection == "ROCK" && computerSelection == "SCISSORS"){
-        console.log("You Win.");
-        player = player + 1;
-        console.log(player);
-        return player;
-    }else if (playerSelection == "SCISSORS" && computerSelection == "PAPER"){
-        console.log("You Win.");
-        player = player + 1;
-        console.log(player);
-        return player;
     }else{
-        console.log("Wrong weapon. computer wins");
-        computer = computer + 1;
-        console.log(computer);
-        return computer
+        chance.textContent = "You Win! rock beats scissors";
+        chance.appendChild = "chance";;
+        player = player + 1;
+        
     }
-}
 
+    let pScore = document.querySelector(".player");
+    pScore.createElement = "p";
+    pScore.textContent = `Player Score: ${player}`;
+    pScore.appendChild = "pScore";
 
-function game (){
-    for (i = 0; i < 5; i++){
-        const playerSelection = chance();
-        const computerSelection = computerPlay();
-        playRound(playerSelection,  computerSelection);
+    let cScore = document.querySelector(".computer");
+    cScore.createElement = "p";
+    cScore.textContent = `Computer Score: ${computer}`;
+    pScore.appendChild = "pScore";
 
+    
+    let result = document.querySelector(".result");
+    result.createElement = "p";
+
+    if(player > 4 ){
+        result.textContent = "You won the game!";
+        result.appendChild = "result";
+        
+    }else if (computer  > 4 ) {
+        result.textContent = "Computer won the game!";
+        result.appendChild = "result";
+         
     }
-    alert(`Your score ${player}`);
-    alert(`computer score ${computer}`);
-    if (player<computer){
-        alert("YOU LOST");
-    }else if(player>computer){
-        alert("YOU WON");
+
+})
+
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", function playRound(playerSelection, computerSelection){
+
+    if(player == 5 ){
+        return;
+        
+    }else if (computer  == 5 ) {
+         return;
+         
+    }
+
+    let chance = document.querySelector(".chances");
+    chance.createElement = "p";
+    
+    
+    playerSelection = "PAPER";
+    console.log(`player:${playerSelection}`);
+    const sign = ["ROCK", "PAPER", "SCISSORS"];
+    const  randomElement = sign[Math.floor(Math.random()* sign.length)];
+    console.log(`computer:${randomElement}`);
+    computerSelection = randomElement;
+    
+     if(computerSelection === "ROCK"){
+        chance.textContent = "You win! paper beats rock";
+        chance.appendChild = "chance";;
+        player = player + 1;
+    
+    }else if(computerSelection === "PAPER"){
+        chance.textContent = "It's a tie. You both chose paper";
+        chance.appendChild = "chance";
     }else{
-        alert("DRAW, PLAY AGAIN");
+        chance.textContent = "You lose! scissors beats paper";
+        chance.appendChild = "chance";;
+        computer = computer + 1;
     }
-}
 
-game();
+    let pScore = document.querySelector(".player");
+    pScore.createElement = "p";
+    pScore.textContent = `Player Score: ${player}`;
+    pScore.appendChild = "pScore";
 
+    let cScore = document.querySelector(".computer");
+    cScore.createElement = "p";
+    cScore.textContent = `Computer Score: ${computer}`;
+    pScore.appendChild = "pScore";
+
+    let result = document.querySelector(".result");
+    result.createElement = "p";
+
+    if(player > 4 ){
+        result.textContent = "You won the game!";
+        result.appendChild = "result";
+        
+    }else if (computer  > 4 ) {
+        result.textContent = "Computer won the game!";
+        result.appendChild = "result";
+         
+    }
+    
+})
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", function playRound(playerSelection, computerSelection){
+
+    if(player == 5 ){
+        return;
+        
+    }else if (computer  == 5 ) {
+         return;
+         
+    }
+
+    let chance = document.querySelector(".chances");
+    chance.createElement = "p";
+   
+    
+    playerSelection = "SCISSORS";
+    console.log(`player:${playerSelection}`);
+    const sign = ["ROCK", "PAPER", "SCISSORS"];
+    const  randomElement = sign[Math.floor(Math.random()* sign.length)];
+    console.log(`computer:${randomElement}`);
+    computerSelection = randomElement;
+    
+   
+   
+    if(computerSelection === "ROCK"){
+        chance.textContent = "You lose! rock beats scissors";
+        chance.appendChild = "chance";
+        computer = computer + 1;
+    }else if(computerSelection == "SCISSORS"){
+        chance.textContent = "It's a tie. You both chose scissors";
+        chance.appendChild = "chance";
+    }else{
+        chance.textContent = "You Win! scissors beats paper";
+        chance.appendChild = "chance";
+        player = player + 1;
+    }
+
+    let pScore = document.querySelector(".player");
+    pScore.createElement = "p";
+    pScore.textContent = `Player Score: ${player}`;
+    pScore.appendChild = "pScore";
+
+    let cScore = document.querySelector(".computer");
+    cScore.createElement = "p";
+    cScore.textContent = `Computer Score: ${computer}`;
+    pScore.appendChild = "pScore";
+
+
+    let result = document.querySelector(".result");
+    result.createElement = "p";
+
+    if(player > 4 ){
+        result.textContent = "You won the game!";
+        result.appendChild = "result";
+        
+    }else if (computer  > 4 ) {
+        result.textContent = "Computer won the game!";
+        result.appendChild = "result";
+         
+    }
+
+})
+
+
+const refresh = document.querySelector("#refresh");
+refresh.addEventListener("click", () => {
+    window.location.reload();
+})
+
+   
 
 
 
